57c9bcaff773ee15dd1403fe38c0c03c0bbfa512
Dan G <daniel.w.gray1994@gmail.com>
2023-10-05 15:26:44 -0400

    added a POST route for the API

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from flask import Flask, request
+import os
+import openai
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
     return "Welcome to the speech-to-text flask app!"
 
+app.route("/create-text", methods=["POST"])
+def transcribe(path)
+    audio_file = open(path, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+
+    return transcript
 
 if __name__ == "__main__":
     app.run(debug=True)
