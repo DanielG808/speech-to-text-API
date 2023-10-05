a8fbaad26a9533f38bb5ed90cf05df9e66c416d1
Dan G <daniel.w.gray1994@gmail.com>
2023-10-05 17:00:51 -0400

    added speech-to-text functionality to a function called transcribe

--- speech-to-text.py
+++ speech-to-text.py
@@ -4,18 +4,19 @@ from dotenv import load_dotenv, dotenv_values
 
 load_dotenv()
 
-API_KEY = os.getenv("API_KEY")
 
-model_id = "whisper-1"
 
-media_file_path = "./assets/test-recording.m4a"
-media_file = open(media_file_path, "rb")
+def transcribe():
+    API_KEY = os.getenv("API_KEY")
+    media_file = open("./assets/test-recording.m4a", "rb")
 
-response = openai.Audio.transcribe(
-    api_key=API_KEY,
-    model=model_id,
-    file=media_file,
-    response_format="text"
-)
+    response = openai.Audio.transcribe(
+        api_key=API_KEY,
+        model="whisper-1",
+        file=media_file,
+        response_format="text"
+    )
+    print(response)
+    return response
 
-print(response)
+transcribe()
